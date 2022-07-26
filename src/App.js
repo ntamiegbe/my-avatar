83389300a025e0b00eabe334bcdd4edafebe7522
Ntami Egbe <ntamiegbe00@gmail.com>
2022-07-26 01:46:34 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import Model from './Model.js'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      camera={{ position: [2, 0, 12.25], fov: 15 }}
+      style={{
+        backgroundColor: '#111a21',
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <ambientLight intensity={1.25} />
+      <ambientLight intensity={0.1} />
+      <directionalLight intensity={0.4} />
+      <Suspense fallback={null}>
+        <Model position={[0.025, -0.9, 0]} /> 
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
   );
 }
 
